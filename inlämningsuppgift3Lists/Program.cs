caa3e6ab6216ba5d1ed53062ecaeaf71cf338f3a
DommSite <johan.johansson06@live.se>
2024-02-02 12:59:57 +0100

    Add project files.

---
+++ inlämningsuppgift3Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace listinlämning_3
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> landlista = new Dictionary<string, int>();
+            landlista.Add("Norge", 385);
+            landlista.Add("Sverige", 450);
+            landlista.Add("Danmark", 43);
+            landlista.Add("Finland", 338);
+
+            Console.WriteLine("Välj ett av länderna:");
+            foreach (KeyValuePair<string, int> item in landlista)
+            {
+                Console.WriteLine(item.Key);
+            }
+                string val = Console.ReadLine();
+            foreach(KeyValuePair<string, int> item in landlista)
+            {
+                if(val==item.Key)
+                {
+                    Console.WriteLine(item.Key + " har arean: " + item.Value);
+                }
+            }
+        }
+    }
+}
